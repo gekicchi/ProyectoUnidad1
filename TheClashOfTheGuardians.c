168c150f7982ce12acf70c8d3398c6ae49add32f
gekicchi <127953506+gekicchi@users.noreply.github.com>
2023-10-15 19:32:14 -0300

    proyecto completo

--- TheClashOfTheGuardians.c
+++ TheClashOfTheGuardians.c
@@ -15,9 +15,10 @@ typedef struct guardian
 
 typedef struct history
 {
-	char result[10];
-	int lives;
-	int turns;
+	char actions[500];
+	int lives_jg;
+	int lives_cpu;
+	int turn;
 	struct history *next;
 } History;
 
@@ -35,18 +36,18 @@ void free_guardians(Guardian *head);
 int get_list_size(Guardian *head);
 
 // History-related functions
-History* create_history(char res[], int li, int tu);
+History* create_history(char act[], int li1, int li2, int tu);
 void add_history(History **head, History **tail, History *add);
 void print_history(History *head);
 void free_history(History *head);
 
 // Game-related functions
 Guardian* create_card(int min[], int max[]);
-void copy_stats(Guardian *deck1, Guardian *deck2, Guardian *copies_head, Guardian *copies_tail);
+void copy_stats(Guardian *deck, Guardian **copies_head, Guardian **copies_tail);
 void shuffle_deck(Guardian **head, Guardian **tail, Guardian **deck_head, Guardian **deck_tail);
 void list_to_list(Guardian **deck_head, Guardian **deck_tail, Guardian **hand_head, Guardian **hand_tail, int pos);
 int look_by_stats(Guardian *head, int mode);
-void attack_guardian(Guardian **defeated_head, Guardian **defeated_tail, Guardian **defender_head, Guardian **defender_tail, Guardian *attacker, int *defender_health, int pos);
+void attack_guardian(Guardian *attacker, Guardian* defender, char movement[]);
 void start_fight(Guardian **list_head, Guardian **list_tail, History **history_head, History **history_tail);
 
 int main(int argc, char *argv[])
@@ -68,14 +69,23 @@ int main(int argc, char *argv[])
 	if (error == 1)
 	{
 		printf("Error: Separe Datos de Archivo con Comas\n");
+		free_guardians(list_head);
 		return 2;
+	}
+	if (error == 2)
+	{
+		printf("Error: Archivo Necesita Minimo 60 Guardianes\n");
+		free_guardians(list_head);
+		return 3;
 	}
 	
 	do{
-		printf("----Menu Provisional----\n");
+		printf("----The Clash of the Guardians----\n");
 		printf("[1] Crear Carta\n[2] Ver Guardianes\n[3] Iniciar Pelea\n[4] Ver Historial\n[5] Salir\n");
-		printf("Eleccion: ");
-		scanf("%d",&election);
+		do{
+			printf("Eleccion: ");
+			scanf("%d",&election);
+		} while (election < 1 || election > 5);
 		printf("\n");
 		
 		switch (election)
@@ -90,6 +100,7 @@ int main(int argc, char *argv[])
 				break;
 				
 			case 3:
+				free_history(history_head);
 				start_fight(&list_head, &list_tail, &history_head, &history_tail);
 				break;
 			
@@ -114,12 +125,15 @@ int read_file(char *file_name, Guardian **head, Guardian **tail, int min[], int 
 {
 	FILE *file = fopen(file_name, "r");
 	char line[100];
-	int data_chunk = 0;
+	int data_chunk = 0, guardian_count = 0;
 	
 	while (fgets(line, 100, file) != NULL)
 	{
 		if (strchr(line, ',') == NULL)
+		{
+			fclose(file);
 			return 1;
+		}
 		
 		char *token;
 		
@@ -151,9 +165,12 @@ int read_file(char *file_name, Guardian **head, Guardian **tail, int min[], int 
 		
 		Guardian *new_guardian = create_guardian(name, type, health, attack, defense);
 		add_guardian(head, tail, new_guardian);
+		guardian_count++;
 	}
 	
 	fclose(file);
+	if (guardian_count < 60)
+		return 2;
 	
 	return 0;
 }
@@ -236,6 +253,8 @@ Guardian* pop_guardian(Guardian **head_stack, Guardian **tail_stack)
 		
 		if (prev != NULL)
 			prev->next = NULL;
+		else
+			*head_stack = NULL;
 			
 		*tail_stack = prev;
 		
@@ -289,20 +308,21 @@ int get_list_size(Guardian* head)
 	
 	while (current != NULL)
 	{
-		current = current->next;
 		count++;
+		current = current->next;
 	}
 	return count;
 }
 
 // creates history structure
-History* create_history(char res[], int li, int tu)
+History* create_history(char act[], int li1, int li2, int tu)
 {
 	History *new_history = malloc(sizeof(History));
 	
-	strcpy(new_history->result, res);
-	new_history->lives = li;
-	new_history->turns = tu;
+	strcpy(new_history->actions, act);
+	new_history->lives_jg = li1;
+	new_history->lives_cpu = li2;
+	new_history->turn = tu;
 	new_history->next = NULL;
 	
 	return new_history;
@@ -326,13 +346,10 @@ void add_history(History **head, History **tail, History *add)
 void print_history(History *head)
 {
 	History *current = head;
-	int count=1;
 	while (current != NULL)
 	{
-		printf("---Combate %d---\n",count);
-		printf("Resultado: %s\nVida Restante: %d\nTurnos Totales: %d\n",current->result, current->lives, current->turns);
+		printf("-----Turno %d-----\nVida Jugador: %d\nVida Cpu: %d\n%s", current->turn, current->lives_jg, current->lives_cpu, current->actions);
 		current = current->next;
-		count++;
 	}
 }
 
@@ -402,30 +419,18 @@ Guardian* create_card(int min[], int max[])
 }
 
 // creates a list of copied guardians from the players decks
-void copy_stats(Guardian *deck1, Guardian *deck2, Guardian *copies_head, Guardian *copies_tail)
+void copy_stats(Guardian *deck, Guardian **copies_head, Guardian **copies_tail)
 {
-	Guardian *deck1_current = deck1;
-	Guardian *deck2_current = deck2;
+	Guardian *deck_current = deck;
 	Guardian *copy = malloc(sizeof(Guardian));
+	int count = 0;
 	
-	while (get_list_size(copies_head) < get_list_size(deck1) + get_list_size(deck2))
+	while (get_list_size(deck) > count)
 	{
-		strcpy(copy->name,deck1_current->name);
-		strcpy(copy->type,deck1_current->type);
-		copy->health = deck1_current->health;
-		copy->attack = deck1_current->attack;
-		copy->defense = deck1_current->defense;
-		add_guardian(&copies_head,&copies_tail,copy);
-		
-		strcpy(copy->name,deck2_current->name);
-		strcpy(copy->type,deck2_current->type);
-		copy->health = deck2_current->health;
-		copy->attack = deck2_current->attack;
-		copy->defense = deck2_current->defense;
-		add_guardian(&copies_head,&copies_tail,copy);
-		
-		deck1_current = deck1_current->next;
-		deck2_current = deck2_current->next;
+		copy = create_guardian(deck_current->name, deck_current->type, deck_current->health, deck_current->attack, deck_current->defense);
+		add_guardian(copies_head,copies_tail,copy);
+		deck_current = deck_current->next;
+		count++;
 	}
 }
 
@@ -498,30 +503,22 @@ int look_by_stats(Guardian *head, int mode)
 }
 
 // attacks guardian and sends it to the defeated list if its health reaches zero
-void attack_guardian(Guardian **defeated_head, Guardian **defeated_tail, Guardian **defender_head, Guardian **defender_tail, Guardian *attacker, int *defender_health, int pos)
+void attack_guardian(Guardian *attacker, Guardian* defender, char movement[])
 {
-	Guardian *defender = *defender_head;
-	int defender_pos = pos;
-	while (pos != 1)
-	{
-		defender = defender->next;
-		defender_pos--;
-	}
-	
 	int damage = attacker->attack - defender->defense;
+	
+	char attacker_name[65], defender_name[65], damage_string[20];
+	strcpy(attacker_name, attacker->name);
+	strcpy(defender_name, defender->name);
+	
 	if (damage <= 0)
 		damage = 1;
-		
 	defender->health -= damage;
-	printf("%s Ataca a %s por %d Puntos de Vida\n", attacker->name, defender->name, damage);
 	
-	if (defender->health <= 0)
-	{
-		printf("%s ya no Puede Combatir\n", defender->name);
-		list_to_list(defender_head,defender_tail,defeated_head,defeated_tail,pos-1);
-		printf("lesgooo\n");
-		*defender_health = *defender_health-1;
-	}
+	itoa(damage,damage_string,10);
+	strcat(movement, strcat(attacker_name, " Ataca a "));
+	strcat(movement, strcat(defender_name, " por "));
+	strcat(movement, strcat(damage_string," Puntos de Vida\n"));
 }
 
 // the fight proper
@@ -531,18 +528,18 @@ void start_fight(Guardian **list_head, Guardian **list_tail, History **history_h
 	Guardian *copies_head = NULL, *copies_tail = NULL, *copy = malloc(sizeof(Guardian));
 	Guardian *jg_head = NULL, *jg_tail = NULL,*jg_hand = NULL, *jg_end = NULL, *jg_arena = NULL, *jg_arena_end = NULL;
 	Guardian *cpu_head = NULL, *cpu_tail = NULL, *cpu_hand = NULL, *cpu_end = NULL, *cpu_arena = NULL, *cpu_arena_end = NULL;
-	Guardian *attacker = NULL, *defeated_head = NULL, *defeated_tail = NULL;
+	Guardian *attacker = NULL, *defender = NULL, *defeated_head = NULL, *defeated_tail = NULL;
 	History *history = malloc(sizeof(History));
 	int jg_lives = 5, cpu_lives = 5;
 	int turn = 1, player_election, cpu_election, to_arena, to_attack, to_defend;
+	char actions[500] = "", decked_name[50] = "", next_turn[5];
 	
 	shuffle_deck(list_head,list_tail,&jg_head,&jg_tail);
 	shuffle_deck(list_head,list_tail,&cpu_head,&cpu_tail);
-	printf("HIIIII!!!!\n");
-	/*
-	copy_stats(jg_head, cpu_head, copies_head, copies_tail);
-	print_guardians(copies_head);
-	*/
+	
+	copy_stats(jg_head, &copies_head, &copies_tail);
+	copy_stats(cpu_head, &copies_head, &copies_tail);
+	
 	while(get_list_size(jg_hand) < 3 && get_list_size(cpu_hand) < 3)
 	{
 		list_to_list(&jg_head,&jg_tail,&jg_hand,&jg_end,-1);
@@ -550,50 +547,68 @@ void start_fight(Guardian **list_head, Guardian **list_tail, History **history_h
 	}
 	
 	do{
-		printf("------Turno %d------\n",turn);
 		printf("--Cartas En Campo--\n");
 		printf("Jugador (%d Vidas):\n",jg_lives);
 		print_guardians(jg_arena);
-		printf("CPU (%d Vidas):\n",cpu_lives);
+		printf("\nCPU (%d Vidas):\n",cpu_lives);
 		print_guardians(cpu_arena);
 		printf("\n");
 		
 		// player turn
 		if (cpu_arena != NULL && jg_arena != NULL)
 		{
-			printf("[1] Bajar Carta\n[2] Atacar\nEleccion: ");
-			scanf("%d",&player_election);
+			printf("[1] Bajar Carta\n[2] Atacar\n");
+			do{
+				printf("Eleccion: ");
+				scanf("%d",&player_election);
+			} while (player_election < 1 || player_election > 2);
 			printf("\n");
 		}
 		else
 			player_election = 1;	
 		
+		strcat(actions, "Accion Jugador:\n");
 		switch(player_election)
 		{	
 			case 1: // hand to arena
 				printf("---Elige Guardian a Bajar---\n");
 				print_guardians(jg_hand);
-				printf("Eleccion: ");
-				scanf("%d",&to_arena);
+				do{
+					printf("Eleccion: ");
+					scanf("%d",&to_arena);
+				} while (to_arena < 1 || to_arena > get_list_size(jg_hand));
+				strcpy(decked_name, see_guardian(jg_hand, to_arena)->name);
+				strcat(actions, strcat(decked_name, " Colocado en la Arena\n"));
 				list_to_list(&jg_hand,&jg_end,&jg_arena,&jg_arena_end,to_arena-1);
 				break;
 				
 			case 2: // attack
 				printf("---Elige Guardian con Quien Atacar---\n");
 				print_guardians(jg_arena);
-				printf("Eleccion: ");
-				scanf("%d",&to_attack);
+				do{
+					printf("Eleccion: ");
+					scanf("%d",&to_attack);
+				} while (to_attack < 1 || to_attack > get_list_size(jg_arena));
 				attacker = see_guardian(jg_arena, to_attack);
 				
 				printf("---Elige Guardian al Que Atacar---\n");
 				print_guardians(cpu_arena);
-				printf("Eleccion: ");
-				scanf("%d",&to_defend);
+				do{
+					printf("Eleccion: ");
+					scanf("%d",&to_defend);
+				} while (to_defend < 1 || to_defend > get_list_size(cpu_arena));
+				defender = see_guardian(cpu_arena, to_defend);
 				
-				attack_guardian(&defeated_head,&defeated_tail, &cpu_arena, &cpu_arena_end, attacker, &cpu_lives, to_defend);
+				attack_guardian(attacker, defender, actions);
+				if (defender->health <= 0)
+				{
+					strcat(actions, strcat(defender->name," ya no Puede Combatir\n"));
+					list_to_list(&cpu_arena,&cpu_arena_end,&defeated_head,&defeated_tail,to_defend-1);
+					cpu_lives--;
+				}
 				break;
 		}
-		printf("\n");
+		strcat(actions, "\nAccion CPU:\n");
 		// cpu turn
 		if (cpu_lives > 0)
 		{
@@ -606,52 +621,71 @@ void start_fight(Guardian **list_head, Guardian **list_tail, History **history_h
 			}
 			else
 				cpu_election = 1;
-			
 			switch(cpu_election)
 			{
 				case 1: // hand to arena
-					printf("Rival Baja Guardian al Campo\n");
 					to_arena = rand()%get_list_size(cpu_hand);
-					list_to_list(&cpu_hand,&cpu_end,&cpu_arena,&cpu_arena_end,to_arena-1);
+					strcpy(decked_name, see_guardian(cpu_hand, to_arena+1)->name);
+					strcat(actions, strcat(decked_name, " Colocado en la Arena\n"));
+					list_to_list(&cpu_hand,&cpu_end,&cpu_arena,&cpu_arena_end,to_arena);
 					break;
 					
 				case 2: // attack
-					printf("Rival Ataca\n");
 					to_attack = look_by_stats(cpu_arena, 2);
-					printf("numero atacante\n");
 					attacker = see_guardian(cpu_arena, to_attack);
-					printf("atacante conseguido\n");
 					
 					to_defend = look_by_stats(jg_arena, 1);
-					printf("numero defensor\n");
-					attack_guardian(&defeated_head,&defeated_tail, &jg_arena, &jg_arena_end, attacker, &jg_lives, to_defend);
+					defender = see_guardian(jg_arena, to_defend);
+					
+					attack_guardian(attacker, defender, actions);
+					if (defender->health <= 0)
+					{
+						strcat(actions, strcat(defender->name," ya no Puede Combatir\n"));
+						list_to_list(&jg_arena,&jg_arena_end,&defeated_head,&defeated_tail,to_defend-1);
+						jg_lives--;
+					}
 					break;
 			}
 		}
-		printf("\n");
+		
+		history = create_history(actions, jg_lives, cpu_lives, turn);
+		add_history(history_head,history_tail,history);
+		printf("\n-----Turno %d-----\n\n%s\nVida Jugador: %d\nVida Cpu: %d\n", history->turn, history->actions, history->lives_jg, history->lives_cpu);
+		strcpy(actions,"");
 		
 		if (turn < 13)
 		{
-			printf("Recogiendo Carta...\n");
+			printf("\nRecogiendo Carta...\n");
 			list_to_list(&jg_head,&jg_tail,&jg_hand,&jg_end,-1);
 			list_to_list(&cpu_head,&cpu_tail,&cpu_hand,&cpu_end,-1);
 		}
+		printf("\n---Fin del Turno---\n\n");
+		printf("Ingrese Cualquier Caracter para Continuar...");
+		scanf("%s",&next_turn);
 		turn++;
-		printf("\n\n");
 	} while (jg_lives > 0 && cpu_lives > 0);
 	
+	
 	if (jg_lives == 0 && cpu_lives == 0)
-		history = create_history("Empate", jg_lives, turn);
+		history = create_history("Empate", jg_lives, cpu_lives, turn);
 	else if (cpu_lives == 0)
-		history = create_history("Victoria", jg_lives, turn);
+		history = create_history("Victoria", jg_lives, cpu_lives, turn);
 	else
-		history = create_history("Derrota", jg_lives, turn);
-		
+		history = create_history("Derrota", jg_lives, cpu_lives, turn);
+	
 	printf("---Resultado Combate---\n");
-	printf("Resultado: %s\nVida Restante: %d\nTurnos Totales: %d\n",history->result, history->lives, history->turns);
+	printf("\n%s\nVida Jugador: %d\nVida Cpu: %d\nTurnos Totales: %d\n", history->actions, history->lives_jg, history->lives_cpu, history->turn);
 	
 	add_history(history_head, history_tail, history);
 	
-	while (get_list_size(copies_head) > 0)
-		list_to_list(&copies_head,&copies_tail, list_head,list_tail,-1);
+	while (get_list_size(copies_head) >= 1)
+		list_to_list(&copies_head, &copies_tail, list_head,list_tail,-1);
+		
+	free_guardians(jg_head);
+	free_guardians(jg_hand);
+	free_guardians(jg_arena);
+	free_guardians(cpu_head);
+	free_guardians(cpu_hand);
+	free_guardians(cpu_arena);
+	free_guardians(defeated_head);
 }
